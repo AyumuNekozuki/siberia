280db9519cde824139a445c1dc67f944144a89dc
AyumuNekozuki <ayumu@nekozuki.me>
2020-08-20 00:12:54 +0900

    code fix

--- common/main.js
+++ common/main.js
@@ -4,21 +4,9 @@ document.getElementById("to_share").onclick = function () {
     input_twiID = "@" + input_twiID + " ";
   }
 
-  //ネット超会議仕様設定
-  var today = new Date(2020, 8, 9, 16, 00, 00);
-  var netchokaigi_summer_start = new Date(2020, 8, 9, 16, 00, 00);
-  var netchokaigi_summer_finish = new Date(2020, 8, 16, 23, 00, 00);
-  var result = "";
-  var resultw = "";
 
-
-  if (today.getTime() <= netchokaigi_summer_finish.getTime()) {
-    result = input_twiID + "お前は #シベリア送り だ！ #ネット超会議2020夏 #ユーザー企画 https://siberia.nekozuki.me/lets-go-siberia";
-    resultw = "https://twitter.com/intent/tweet?text=" + input_twiID + "お前は%20%23シベリア送り%20だ！%20%23ネット超会議2020夏%20%23ユーザー企画%20&url=https%3A%2F%2Fsiberia.nekozuki.me%2Flets-go-siberia";
-  } else {
-    result = input_twiID + "お前は #シベリア送り だ！https://siberia.nekozuki.me/lets-go-siberia";
-    resultw = "https://twitter.com/intent/tweet?text=" + input_twiID + "お前は%20%23シベリア送り%20だ！&url=https%3A%2F%2Fsiberia.nekozuki.me%2Flets-go-siberia";
-  }
+  result = input_twiID + "お前は #シベリア送り だ！https://siberia.nekozuki.me/lets-go-siberia";
+  resultw = "https://twitter.com/intent/tweet?text=" + input_twiID + "お前は%20%23シベリア送り%20だ！&url=https%3A%2F%2Fsiberia.nekozuki.me%2Flets-go-siberia";
 
   document.getElementById("sharebox").innerHTML = result;
   document.getElementById("tw_sharebut").href = resultw;
--- lets-go-siberia/redirect.js
+++ lets-go-siberia/redirect.js
@@ -1,17 +1,3 @@
-//ネット超会議仕様設定
-var today = new Date();
-var netchokaigi_summer_start = new Date(2020, 8, 9, 16, 00, 00);
-var netchokaigi_summer_finish = new Date(2020, 8, 16, 23, 00, 00);
-var netchokaigi = "";
-
-if(today.getTime() >= netchokaigi_summer_start.getTime()){
-  if(today.getTime() <= netchokaigi_summer_finish.getTime()){
-    netchokaigi = "Yes";
-    document.getElementById("default").classList.add("hide");
-    document.getElementById("netchokaigi").classList.remove("hide");
-  }
-}
-
 // カウントダウンする秒数
 var sec = 7;
  
@@ -24,17 +10,11 @@ var cnt = sec;
 var id = setInterval(function(){
   cnt--;
   document.getElementById("count").innerHTML = cnt;
-  document.getElementById("count2").innerHTML = cnt;
   dt = new Date();
   if(dt.getTime() >= endDt.getTime()){
     clearInterval(id);
 
-    //redirect
-    if(netchokaigi == "Yes"){
-        location.href = "https://chokaigi.jp/2020summer/plan/tetsudo.html";
-    }else{
-      location.href = "https://www.google.co.jp/maps/place/ロシア+シベリア/@52.2869741,104.3050183,3a,75y/data=!3m8!1e2!3m6!1sAF1QipMH4kA87J5_xoCWls6fvz3y039Uia3WZ8oGxdl4!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMH4kA87J5_xoCWls6fvz3y039Uia3WZ8oGxdl4%3Dw114-h86-k-no!7i1920!8i1439!4m5!3m4!1s0x5c87ebbaf5e77a3b:0x2c4412982b67464c!8m2!3d61.0137097!4d99.1966559";
-    }
+    location.href = "https://www.google.co.jp/maps/place/ロシア+シベリア/@52.2869741,104.3050183,3a,75y/data=!3m8!1e2!3m6!1sAF1QipMH4kA87J5_xoCWls6fvz3y039Uia3WZ8oGxdl4!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMH4kA87J5_xoCWls6fvz3y039Uia3WZ8oGxdl4%3Dw114-h86-k-no!7i1920!8i1439!4m5!3m4!1s0x5c87ebbaf5e77a3b:0x2c4412982b67464c!8m2!3d61.0137097!4d99.1966559";
 
   }
 }, 1000)
