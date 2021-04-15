4692b1f7a77c392866b0b0b99fd7149b80c1385b
AyumuNekozuki <ayumu@nekozuki.me>
2021-04-16 01:50:11 +0900

    chokaigi2021

---
+++ common/firebase_init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7h_3dRTp7AwreP1Xz-pQ3LDa3_HNfZeA",
+    authDomain: "siberia-okuri.firebaseapp.com",
+    projectId: "siberia-okuri",
+    storageBucket: "siberia-okuri.appspot.com",
+    messagingSenderId: "449484741078",
+    appId: "1:449484741078:web:0b016bb03253d6ffdcec77",
+    measurementId: "G-S57BDBLH3C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
